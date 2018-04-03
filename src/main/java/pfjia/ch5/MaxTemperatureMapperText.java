9eb2e59b2f2536f4adc6bbf842b5370981ae3c4f
pfjia <2431631881@qq.com>
2018-04-04 00:30:50 +0800

    日常提交

---
+++ src/main/java/pfjia/ch5/MaxTemperatureMapperText.java
@@ -0,0 +1,14 @@
+package pfjia.ch5;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pfjia
+ * @since 2018/4/4 0:20
+ */
+public class MaxTemperatureMapperText {
+    @Test
+     void processesValidRecord(){
+        new MapDriver<>()
+    }
+}
